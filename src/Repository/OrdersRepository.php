e97e9192e68533d2ba48e87561ff0b42e28d2c24
Mohamed Testouri <60431478+MohamedTestouri@users.noreply.github.com>
2021-05-07 07:45:57 +0100

    Add the OrderRepository and the PackRepository

---
+++ src/Repository/OrdersRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Orders;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Orders|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Orders|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Orders[]    findAll()
+ * @method Orders[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class OrdersRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Orders::class);
+    }
+
+
+public function myOrders(int $id){
+        return $this->createQueryBuilder('p')
+            ->select('p.idpack as pack')
+            ->where('p.iduser = :idUser')
+            ->setParameter('idUser', $id)
+            ->getQuery()->getResult();
+}
+    public function getMyOrders(int $id){
+        return $this->createQueryBuilder('p')
+            ->where('p.iduser = :idUser')
+            ->setParameter('idUser', $id)
+            ->getQuery()->getResult();
+    }
+    public function showMyOrders(int $id, String $params){
+        return $this->createQueryBuilder('p')
+            ->select('p.idpack as pack')
+            ->where('p.iduser = :idUser')
+            ->setParameter('idUser', $id)
+            ->andWhere('p.status = :status')
+            ->setParameter('status', $params)
+            ->getQuery()->getResult();
+    }
+
+public function deleteOrder(int $id){
+      $this->createQueryBuilder('o')
+    ->delete()
+    ->where('o.idpack = :idPack')
+    ->setParameter('idPack', $id)
+      ->getQuery()->getResult();
+}
+    public function countOrdersByStatus()
+    {
+        return $this->createQueryBuilder('p')
+            ->select('count(p.id) as nombre', 'p.status as status')
+            ->groupBy('p.status')
+            ->getQuery()
+            ->getResult();
+    }
+}
